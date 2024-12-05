74b106a2b11c0852c5372f6efe80837b61d9d2f2
Levon Aslanyan <aslanyan2012@mail.ru>
2024-12-05 19:07:59 +0400

    Header Working

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,1 @@
-export { Header } from "./Header";
-export { Footer } from "./Footer";
-export { Card } from "./molecules/Card";
+
--- src/routes/Allroutes.jsx
+++ src/routes/Allroutes.jsx
@@ -11,8 +11,8 @@ const Allroutes = () => {
         <Route path="/" element={<MovieList />} />
         <Route path="movie/:id" element={<MovieDetail />} />
         <Route path="movies/popular" element={<MovieList />} />
-        <Route path="movies/top " element={<MovieList />} />
-        <Route path="movies/upcoming " element={<MovieList />} />
+        <Route path="movies/top" element={<MovieList />} />
+        <Route path="movies/upcoming" element={<MovieList />} />
         <Route path="search" element={<Search />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
